c4da4dcb9b171c93abf1f30eb25764b0551a807d
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-09-10 10:17:25 +0200

    add labels

--- js/components/ContactForm.js
+++ js/components/ContactForm.js
@@ -6,17 +6,23 @@ var ContactForm = React.createClass({
   render: function() {
     return (
       React.createElement('form', {className: 'contactForm'},
+        React.createElement('label', {for: 'firstName'}),
         React.createElement('input', {
+          id: 'firstName',
           type: 'text',
           placeholder: 'Imię',
           value: this.props.contact.firstName,
         }),
+        React.createElement('label', {for: 'lastName'}),
         React.createElement('input', {
+          id: 'lastName',
           type: 'text',
           placeholder: 'Nazwisko',
           value: this.props.contact.lastName,
         }),
+        React.createElement('label', {for: 'email'}),
         React.createElement('input', {
+          id: 'email',
           type: 'email',
           placeholder: 'Email',
           value: this.props.contact.email,
